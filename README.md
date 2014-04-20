6d26e58ec21f981cb0932e8373260f90609a4a00
Jawish Hameed <jaa@semicolon.com.mv>
2014-04-20 18:22:21 +0500

    Initial commit to git

---
+++ README.md
@@ -0,0 +1,79 @@
+Javascript Thaana Keyboard is a brkeyboard handler for Javascript browser environments that translates key input into the appropriate Unicode Thaana characters. Hence, it makes it possible for HTML text input and textarea fields (and similar) to accept Thaana without having to require the user to switch the keyboard language on their computer. Such a feature contributes for a better user experience as the user can simply enter Dhivehi in Thaana without the extra hassle.
+
+## Keyboard support
+JTK now supports the following keyboard layouts:
+
+Phonetic (Segha version): This keyboard is perhaps the most popular Thaana keyboard layout. JTK identifies this keyboard as "phonetic".
+
+Phonetic (Hassan Hameed version): This keyboard is similar to above but notably differs in its mapping of alifu, abafili, aabaafili, gaafu and the sukun. JTK identifies this keyboard as 'phonetic-hh'.
+
+Typewriter: This is the standardized Thaana layout used on typewriters. JTK identifies this keyboard as 'typewriter'.
+
+## Browser support
+JTK should work perfectly well on:
+- Microsoft Internet Explorer 5.5+
+- Mozilla Firefox 1+
+- Opera 9+
+- Apple Safari 2+
+- Google Chrome 0.1+.
+
+## Basic usage
+The basic usage allows for fast and easy integration of JTK on your Thaana web pages.
+
+1. Link the file in the HEAD section of the page:
+```html
+<script type="text/javascript" src="jtk-4.0.packed.js"></script>
+```
+
+2. For any element accepting input (i.e. INPUTs, TEXTAREAs, content-editable DIVs), assign them the special class name "thaanaKeyboardInput". JTK will automatically handle text entry to any element with that class name. You can assign further classes to the elements without ill-effect, if needed.
+
+3. There are two ways to set the keyboard used for an element.
+
+### defaultKeyboard method
+This method allows setting a default keyboard to be used on all elements handled by JTK. To do this, add the following to the HEAD section of your web page but make sure it is added after the code inserted from step 1 above.
+
+```html
+<script type="text/javascript">
+// Default mode: 'off', 'phonetic', 'phonetic-hh', 'typewriter'
+thaanaKeyboard.defaultKeyboard = 'phonetic';
+</script>
+```
+
+### thaanaKeyboardState method
+This method allows per element control on the type of keyboard used by an element handled by JTK. To do this, add a form element (can be a radio, checkbox, select, hidden or text field) with its name set to the text entry element id suffixed with the string "_thaanaKeyboardState". The value of these fields should specify either 'off', 'phonetic', 'phonetic-hh' or 'typewriter', indicating the status and the keyboard in use.
+
+So, if you had a text entry field with the id "fullname" then the keyboard could be specified using a hidden field as follows:
+```html
+<input type="text" class="thaanaKeyboardInput" id="fullname">
+<input type="hidden" name="fullname_thaanaKeyboardState" value="phonetic">
+```
+
+4. Make sure that the text direction for the Thaana fields is set to "rtl". This can be easily achieved using CSS, by adding a class definition for the "thaanaKeyboardInput" class or by any other method of your choice. Adding the following to your CSS definition should suffice for most uses:
+```css
+.thaanaKeyboardInput {
+    font-family: faruma, 'mv iyyu nala', 'mv elaaf normal';
+    direction: rtl;
+}
+```
+
+If the above instructions are followed correctly, the JTK Thaana functionality would be in effect soon as the page has loaded!
+
+## Advanced Usage: The JTK object, methods and properties
+To facilitate advanced integration functionality for developers looking to have (finer) control over its behavior, JTK now makes itself available as a public object named "thaanaKeyboard". 
+
+The following properties and methods exposed by the "thaanaKeyboard" object:
+
+defaultKeyboard: [property] The Thaana keyboard layout type to default to when JTK enabled elements do not have a keyboard specified.
+Valid values are: 'off' to keep Thaana disabled, 'phonetic' to use the standard phonetic layout, 'phonetic-hh' to use the phonetic layout by Dr. Hassan Hameed and 'typewriter' to use the typewriter layout.
+
+setHandlerById ( id, action ): [method] Sets the state of the JTK handler for a page element.
+The id argument should be a string containing the id of any content-editable element. The action argument should specify either "enable" or "disable" depending on whether input handling for Thaana should be enabled or disabled, respectively.
+
+setHandlerByClass ( class, action ): [method] Sets the state of the JTK handler for a set of page elements.
+The class argument should be a string containing the class name of any content-editable element (i.e input, textarea etc). The action argument should specify either "enable" or "disable" depending on whether input handling for Thaana should be enabled or disabled, respectively.
+
+## Demo
+Demo: http://www.jawish.org/blog/uploads/jtk-4.2.1-test.html
+
+## License
+JTK is released under the MIT License, allowing its use in both personal and commercial applications as long as the copyright and license permission notice remains intact.
---
+++ jtk.js
@@ -0,0 +1,200 @@
+﻿/*
+ * Javascript Thaana Keyboard 4.2.1
+ *
+ * Copyright (c) 2009 Jawish Hameed (jawish.org)
+ * Licensed under the MIT license.
+ */
+
+var thaanaKeyboard = {
+	
+	// Default keyboard (used only when not explicitly declared using state)
+	defaultKeyboard: '',
+	
+	// Setup Ascii Thaana -> Unicode translation matrix
+	_transFrom: 'qwertyuiop[]\\asdfghjkl;\'zxcvbnm,./QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?()',
+	_transToKbd: {	'phonetic': 'ްއެރތޔުިޮޕ][\\ަސދފގހޖކލ؛\'ޒ×ޗވބނމ،./ޤޢޭޜޓޠޫީޯ÷}{|ާށޑﷲޣޙޛޚޅ:\"ޡޘޝޥޞޏޟ><؟)(',
+			'phonetic-hh': 'ޤަެރތޔުިޮޕ][\\އސދފގހޖކލ؛\'ޒޝްވބނމ،./ﷲާޭޜޓޠޫީޯޕ}{|ޢށޑޟޣޙޛޚޅ:\"ޡޘޗޥޞޏމ><؟)(',
+			'typewriter': 'ޫޮާީޭގރމތހލ[]ިުްަެވއނކފﷲޒޑސޔޅދބށޓޯ×’“/:ޤޜޣޠޙ÷{}<>.،\"ޥޢޘޚޡ؛ޖޕޏޗޟޛޝ\\ޞ؟)('
+			},
+	
+	// Default class name used for hooking at initialization
+	_className: 'thaanaKeyboardInput',
+	
+	
+	/**
+	 * Apply input handling to elements with the special JTK class name
+	 */
+	init: function () {
+		this.setHandlerByClass(this._className, 'enable');
+	},
+	
+	/**
+	 * Set key event handlers for elements with the specified id
+	 * 
+	 * @param id		String id of the element to affect
+	 * @param action	String specifying action to take. Should be 'enable' or 'disable'.
+	 */
+	setHandlerById: function (id, action) {
+		// Get the element object
+		var elem = document.getElementById(id);
+		
+		// Respond to action specified
+		switch (action) {
+			case 'enable':
+				// Enable input handling
+				elem.onkeypress = this.handleKey;
+				
+				break;
+				
+			case 'disable':
+				// Disable input handling
+				elem.onkeypress = null;
+				
+				break;
+		}
+	},
+	
+	/**
+	 * Set key event handlers for elements of the specified class
+	 * 
+	 * @param clsname	String class name of the elements to affect
+	 * @param action	String specifying action to take. Should be 'enable' or 'disable'.
+	 */
+	setHandlerByClass: function (clsname, action) {
+		// Setup regular expression for finding the target class
+		var rePat = new RegExp('\\b' + clsname + '\\b');
+		
+		// Get all the elements on page
+		var elemList = document.all || document.getElementsByTagName('*');
+		
+		// Loop through the elements
+		for (n = 0; n < elemList.length; n++) {
+			// Ensure the current element is of the target class
+			if (rePat.test(elemList[n].className)) {
+				// Set handler
+				this.setHandlerById(elemList[n].id, action);
+			}
+		}
+	},
+	
+	/**
+	 * Do character translation on key activity
+	 */
+	handleKey: function (e) {		
+		// Get the event data
+		if (!e) var e = window.event;
+		
+		// Get the pressed key
+		if (e.which == null) {
+			// IE:
+			keycode = e.keyCode;
+		}
+		else if (e.which > 0) {
+			// Non-IE printable chars:
+			keycode = e.which;
+			
+			// Handle special keys misfire in Opera
+			if (window.opera && [45, 46, 35, 36].indexOf(keycode) == 1) return true;
+		}
+		else {
+			// Ignore special keys
+			return true;
+		}
+		
+		// Check for CTRL modifier key
+		if (e.modifier) {
+			var ctrl = e.modifiers & Event.CONTROL_MASK;
+		}
+		else if (typeof(e.ctrlKey) != 'undefined') {
+			var ctrl = e.ctrlKey;
+		}
+		
+		// Get translation details for the entered char
+		try {
+			var transIndex = thaanaKeyboard._transFrom.indexOf(String.fromCharCode(keycode));
+			
+			// If pressed key does not require translation, let default action proceed
+			if (transIndex == -1 || ctrl) return true;
+			
+			
+			// Set default state
+			var state = thaanaKeyboard.defaultKeyboard;
+			
+			// Get state
+			var elemState = document.getElementsByName(this.id + '_thaanaKeyboardState');
+			if (elemState) {
+				// State element has been defined:
+				
+				// Get the state specified by the element
+				for (i = 0; i < elemState.length; i++) {
+					if (elemState[i].type == 'radio' || elemState[i].type == 'checkbox') {
+						// Handle radio/checkbox fields
+						if (elemState[i].checked == true) {
+							state = elemState[i].value;
+							break;
+						}
+					}
+					else if (elemState[i].type == 'select-one') {
+						// Handle select/combo list fields
+						state = elemState[i].options[elemState[i].selectedIndex].value;
+						break;
+					}
+					else if (elemState[i].type == 'hidden' || elemState[i].type == 'text') {
+						// Handle hidden and text fields
+						state = elemState[i].value;
+						break;
+					}
+					
+				}
+			}
+			
+			// Look up the translated char
+			var transChar = thaanaKeyboard._transToKbd[state].substr(transIndex, 1);
+		}
+		catch (err) {
+			// Incorrect type or type not defined, return immediately
+			return;
+		}
+		
+		// Cancel default action for the key
+		if (typeof e.preventDefault == 'function') {
+			e.preventDefault();
+		}
+		else {
+			e.returnValue = false;
+		}
+		
+		// Store scroll position for textarea
+		var sPos = this.scrollTop;
+		
+		// Insert text at cursor position
+		if (this.selectionStart) {
+			// For Firefox/Safari:
+			var selOld = this.selectionStart + 1;
+			this.value = this.value.substring(0, this.selectionStart) 
+						+ transChar 
+						+ this.value.substring(this.selectionEnd, this.value.length);
+			
+			this.setSelectionRange(selOld, selOld);
+			this.focus();
+		}
+		else if (document.selection) {
+			// For IE/Opera:
+			sel = document.selection.createRange();
+			sel.text = transChar;
+			this.focus();
+		}
+		else {
+			// If all fails, just append
+			this.value += transChar;
+		}
+		
+		// Restore scroll position for textarea
+		this.scrollTop = sPos;
+	}
+
+};
+
+// Start the Thaana Keyboard soon as the page is loaded
+var old = (window.onload) ? window.onload : function () {};
+window.onload = function(){old(); thaanaKeyboard.init();};
